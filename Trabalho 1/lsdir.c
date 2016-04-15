5ba2e1d6a51fbc68bdba99ab4954695f28c268ae
jazzchipc <josealeix.pc@outlook.com>
2016-04-15 23:01:12 +0100

    lsdir has file struct and get info function working

--- Trabalho 1/lsdir.c
+++ Trabalho 1/lsdir.c
@@ -1,25 +1,56 @@
 #include <stdio.h>
-#include <dirent.h>	/*For directory related functions (DIRectory ENTries)*/
+/*For directory related functions (DIRectory ENTries)*/
+#include <dirent.h>	
 
 /*stat function includes*/
 /*See: http://linux.die.net/man/2/stat*/
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+/*for time functions and structs*/
+#include <time.h>
+
+
+/*
+ * Struct that'll be used to store regular files' properties. 
+ * */
+typedef struct 
+{
+	char* name;			// file's name
+	long long size;		// file's size in bytes
+	unsigned long mode;	// file permissions in decimal representation
+	time_t lastMDate;	// last file modification date in time_t struct
+	char* absolutePath;	// file's absolute path in the system
+}RFile;
 
 /*TODO: TO GET FILE INFORMATION USE STAT() FUNCTION*/
 
-int getInfoFromFile (char* filePath)
+/* Saves a file's info to a RFile struct. DON'T FORGET TO FREE MEMORY AFTER USING IT! @see RFile.
+ * */
+RFile* getInfoFromFile (char* filePath)
 {
-	/*st_mode also has the permissions*/
-	struct stat *s;
-	int ret;
+	struct stat s;		// get file data
+	RFile* file = (RFile*)malloc(sizeof(RFile));	// store file data
 	
-	stat (filePath, s);
+	stat (filePath, &s);
 	
-	/*Use "/" do separate data.*/
+	file->size = (long long) s.st_size;
+	file->mode = (unsigned long) (s.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO));
+	file->lastMDate = s.st_mtime;	
+	file->absolutePath = filePath;
 	
-	ret = (int)s->st_mode;
+	return file;
+}
+
+/* Converts RFile data to a string that can be saved in a .txt file.
+ * */
+char* rFileToString(RFile file)
+{
+	char* ret = (char*)malloc(sizeof(char));
+	
+	sprintf(ret, "%s %lld %lo %s %s", file.name, file.size, file.mode, file.absolutePath, ctime(&file.lastMDate));	// %lo prints in long octal
 	
 	return ret;
 }
@@ -62,10 +93,17 @@ int writeFilesFromDir(char* directory){
 int main()
 {
 	char* testDirectory = "/usr/users2/mieic2014/up201403526/Downloads";
-	char* filePath = "/usr/users2/mieic2014/up201403526/Downloads/test";
+	char* filePath = "/home/jazz/Desktop/SOPE-Feup/Trabalho 1/rmdup.c";
+	
+	time_t t = time(NULL);
+	
+	RFile* testfile = getInfoFromFile(filePath);
 	
 	//writeFilesFromDir(testDirectory);
 	
-	printf ("%d", getInfoFromFile(filePath));
+	char* c = rFileToString(*testfile);	// since the pointer becomes a local variable in this main() function, it doesn't need to be freed (???) -> if freed gives error
+	
+	printf ("%s", c);
+	
 	
 }
---
+++ Trabalho 1/rmdup.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*For directory related functions (DIRectory ENTries)*/
+#include <dirent.h>	
+
+/*stat function includes*/
+/*See: http://linux.die.net/man/2/stat*/
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+	
+}
