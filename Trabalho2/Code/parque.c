a5685508fdc713b541700caed135130c82fae7cb
jazzchipc <josealeix.pc@outlook.com>
2016-05-24 01:07:02 +0100

    Cat's version of Parque.

--- Trabalho2/Code/parque.c
+++ Trabalho2/Code/parque.c
@@ -1,110 +1,253 @@
-#include <stdio.h> 
-#include <unistd.h> 
-#include <pthread.h> 
-#include <sys/stat.h> 
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
 #include <time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include  <fcntl.h>
+#include <stdlib.h>
+#include <math.h>
 
 #define FIFO_READ 0
-#define FIFO_WRITE 1 
+#define FIFO_WRITE 1
 
-#define STDERR 2 
+#define STDERR 2
 #define NUMITER 10000
 
-pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;	// initialization of mutex
+#define NUM_OF_CONTROLLERS 4
 
-int numParkingSpaces, openingTime;
+#define LOGFILE "parque.log"
 
-typedef struct vehicleInfo
-{
+char directions[4] = "NSWE";
+
+// initialization of mutex
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutWrite = 	PTHREAD_MUTEX_INITIALIZER;
+
+
+int numParkingSpaces, openingTime, freeSpaces, closed;
+time_t beginningTime;
+
+typedef struct{
 	char entryDoor;
-	time_t parkingTime;
+	clock_t parkingTime;
 	int vehicleID;
 	char* fifoName;
-	
+
 } vehicleInfo;
+
+/*
+*/
+int myTime(int tics){
+	return (tics/sysconf(_SC_CLK_TCK)*pow(10,6));
+}
+
+char * getFifoName (char *direction){
+	char fifoName[] = "/tmp/fifo";
+	sprintf(fifoName, "%s%s", fifoName, direction);
+	return fifoName;
+}
+
+//Write to file t(ticks) ; nlug ; id_viat ; observ
+int writeToFile(char * message, int id){
+	FILE *file;
+
+	file = fopen(LOGFILE, "w");
+
+	if (file == NULL){
+		printf("\nError opening file %s.", LOGFILE);
+		return -1;
+	}
+
+	time_t actualTime = time(NULL);
+	clock_t timeToWrite = (actualTime - beginningTime) * CLOCKS_PER_SEC;
+
+	int spaces = numParkingSpaces - freeSpaces;
+
+	fprintf(file, "%d ; %d ; %d ; %s", (int)timeToWrite, spaces, id, message);
+
+	return 0;
+}
+
+void WriteInFileLog(char * message, int id){
+	pthread_mutex_lock(&mutWrite);
+	writeToFile(message, id);
+	pthread_mutex_unlock(&mutWrite);
+}
+
+/*
+* Decrements the number of free spaces
+*/
+void occupyParkingSpace(){
+	pthread_mutex_lock(&mut);
+	freeSpaces--;
+	pthread_mutex_unlock(&mut);
+}
+
+/*
+* Increments the number of free spaces
+*/
+void freeParkingSpace(){
+	pthread_mutex_lock(&mut);
+	freeSpaces++;
+	pthread_mutex_unlock(&mut);
+}
+
 
 
 /*
 *	Creates valet to accompany vehicle to a free parking space or an exit
 */
-void * valet (void *arg);
+void * valet (void *arg){
+	vehicleInfo info = *(vehicleInfo *) arg;
 
-/*
-*	Creates controller
-*/
-void *controller (void *direction){
+	char * message = (char *) malloc(100);
+	int admission;
 
-	char fifoname[] = "/tmp/fifo";
-	sprintf(fifoName, "%s%c", fifoName, (char *)direction);
-	
-	struct stat st;
-	
-	// If fifo does not exist, create one
-	if (stat(fifoName, &st) != 0){	
-		//If an error occurs when creating fifo
-   		if (mkfifo(fifoname, 0666) == -1)
-			printf("\nError creating fifo.\n");
-	}
-	
-	//Opens fifo to read and write
-	int fd = open(fifoname, O_RDWR); //or O_RDONLY (read only)
-	
+	char fifoName[] = "/tmp/vehicle";
+	sprintf(fifoName, "%s%c", fifoName, info.vehicleID);
+
+	//Opens fifo to write
+	int fd = open(fifoName, O_WRONLY);
+
 	if (fd == -1){
 		perror("Opening file");
 		exit(-1);
 	}
 
-	c = getchar();
-	size_t size = sizeof(char) * BUF_SIZE;
-	char * buffer = malloc(size);
-	
+	if (freeSpaces == 0){
+		message = "full";
+		admission = 0;
+	}
+	else{
+		message = "entry";
+		admission = 1;
+		occupyParkingSpace();
+	}
+
+	//sends message to vehicle
+	write(fd, message, sizeof(message));
+	//writes in file .log
+	WriteInFileLog(message, info.vehicleID);
+
+	//if vehicle can enter the parking lot
+	if (admission == 1){
+		//CONFIRMAR
+		usleep(myTime(info.parkingTime));
+		//sleep(info.ParkingTime/CLOCKS_PER_SEC);
+
+		//when the time runs out
+		message="exit";
+		write (fd, message, sizeof(message));
+		freeParkingSpace();
+		WriteInFileLog(message, info.vehicleID);
+	}
+
+	pthread_exit(0);
+}
+
+/*
+*	Creates controller
+*/
+void *controller (void *arg){
+	char direction = * (char *) arg;
+
+	time_t end = time(NULL);
+	time_t diff = end - beginningTime;
+	if (diff == openingTime)
+		closed = 1;
+
+	char * fifoName = getFifoName(direction);
+
+	struct stat st;
+
+	// If fifo does not exist, create one
+	if (stat(fifoName, &st) != 0){
+		//If an error occurs when creating fifo
+   		if (mkfifo(fifoName, 0666) == -1)
+			printf("\nError creating fifo.\n");
+	}
+
+	//Opens fifo to read
+	int fd = open(fifoName, O_RDONLY);
+
+	if (fd == -1){
+		perror("Opening file");
+		exit(-1);
+	}
+
 	char dir;
-	clock_t time;
+	clock_t timeToStay;
 	int id;
 	vehicleInfo vehicle;
 
-	while (c != EOF) {
+	while (closed == 0){
+		char * line = NULL;
+		size_t len = 0;
+		while (getline(&line, &len, fdopen(fd, "r")) == -1) {
 
-		//Receives information about a request from a new client
+			//Receives information about a request from a new client
+			sscanf(line, "%c %d %d", &dir, (int *)timeToStay, &id);
 
- 		getline(&buffer,&size,fdopen(fd, "r"));
-		sscanf(buffer, "%c %l %d", &dir, &time, &id);
-	
-		vehicle.entryDoor = dir;
-		vehicle.parkingTime = time;
-		vehicle.vehicleID = id;
-		vehicle.fifoName = fifoName;
+			vehicle.entryDoor = dir;
+			vehicle.parkingTime = timeToStay;
+			vehicle.vehicleID = id;
+			vehicle.fifoName = fifoName;
 
-		pthread_t val;
+			//Creates detached thread "Valet"
+			pthread_attr_t attr;
+			pthread_attr_init(&attr);
+			pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+			pthread_t val;
+			pthread_create(&val, &attr, valet, &vehicle);
+			//pthread_attr_destroy(&attr);
 
-		pthread_create(valet, NULL, valet, vehicle);
+		}
+		end = time(NULL);
+		if (diff == openingTime)
+			closed = 1;
+	}
 
-		//cria arrumador para passar informacao do veiculo e este ser acompanhado a um lugar ou à saída
-    	c = getchar();
-  	}
-
+	//ler os pedidos pendentes, fechar fifo, e notificar os clientes do encerramento
+	close(fd);
+	remove(fifoName);
+	pthread_exit(0);
 
 }
 
 int main(int argc, char* argv[]) {
-	
-	if (argc != 3)
-	{
+
+	if (argc != 3){
 		 printf("Usage: %s <nr_pos> <nr_thrs>\n", argv[0]);
+		 printf("Usage: parque <nr_pos> <nr_thrs>\n");
 		 return 1;
 	}
-	
-	numParkingSpaces = atoi(argv[1]);	// number of parking spaces
-	openingTime = atoi(argv[2]);		// duration of the parking lot in SECONDS
-	
+
+	//Initializes global variables
+	numParkingSpaces = abs(atoi(argv[1]));	// number of parking spaces
+	openingTime = abs(atoi(argv[2]));		// duration of the parking lot in SECONDS
+	freeSpaces = numParkingSpaces;
+	closed = 0;
+	beginningTime = time(NULL); //returns the time in seconds since 1970...
+
 	pthread_t contN, contS, contW, contE;
 
-	//Cria os controladores de cada entrada/saída do parque
-	pthread_create(&contN, NULL, controller, 'N'); //North
-	pthread_create(&contS, NULL, controller, 'S'); //South
-	pthread_create(&contW, NULL, controller, 'W'); //West
-	pthread_create(&contE, NULL, controller, 'E'); //East
+	//Creates the threads "Controller" of the park
+	pthread_create(&contN, NULL, &controller, &directions[0]); //North
+	pthread_create(&contS, NULL, &controller, &directions[1]); //South
+	pthread_create(&contW, NULL, &controller, &directions[2]); //West
+	pthread_create(&contE, NULL, &controller, &directions[3]); //East
 
-	return 0;
+	sleep(openingTime);
+
+	int i;
+	for (i = 0; i < NUM_OF_CONTROLLERS; i++){
+		char * fifoName = getFifoName(&directions[i]);
+		char message[] = "closed";
+		int fd = open(fifoName, O_WRONLY);
+		write(fd, &message, sizeof(message));
+		close(fd);
+	}
+
+	pthread_exit(0);
 }
-
